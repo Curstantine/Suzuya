b1cfe99faa11a71f8ab1558633fb35b3671b82be
Curstantine <resistlyfe@gmail.com>
2021-10-28 22:31:42 +0530

    Added: editCoverArt, deleteCoverArt

--- src/utils/cover/index.ts
+++ src/utils/cover/index.ts
@@ -4,8 +4,8 @@ import FormData from "form-data";
 import Auth from "../auth/auth";
 import Config from "../../config";
 import Helper from "../../extra/helper";
-import { CoverQueryParameters, CoverUploadBody } from "./types";
-import { CollectionResponse, EntityResponse } from "../../extra/common";
+import type { CoverQueryParameters, CoverUploadBody } from "./types";
+import type { CollectionResponse, EntityResponse, Response } from "../../extra/common";
 
 export default class Cover {
   private readonly auth: Auth;
@@ -65,6 +65,49 @@ export default class Cover {
     if (response.status >= 400) throw new Error(`${response.statusText} [${response.status}]`);
 
     const data: EntityResponse<"cover_art"> = await response.json();
+    return data;
+  }
+
+  /**
+   * **Requires Authentication**
+   *
+   * [Documentation](https://api.mangadex.org/docs.html#operation/edit-cover)
+   */
+  public async editCoverArt(coverId: string, body: CoverUploadBody) {
+    const url = new URL(`${this.config.APIUrl}/cover/${coverId}`);
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorizations: `Bearer ${this.auth.Cache.session}`,
+      },
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
+    if (response.status >= 400) throw new Error(`${response.statusText} [${response.status}]`);
+
+    const data: EntityResponse<"cover_art"> = await response.json();
+    return data;
+  }
+
+  /**
+   * **Requires Authentication**
+   *
+   * [Documentation](https://api.mangadex.org/docs.html#operation/delete-cover)
+   */
+  public async deleteCoverArt(coverId: string) {
+    const url = new URL(`${this.config.APIUrl}/cover/${coverId}`);
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorizations: `Bearer ${this.auth.Cache.session}`,
+      },
+      method: "DEL",
+    });
+    if (response.status >= 400) throw new Error(`${response.statusText} [${response.status}]`);
+
+    const data: Response = await response.json();
     return data;
   }
 }
