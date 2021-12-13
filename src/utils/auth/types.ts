7e1a04139911cbc9c388db4ba98d21c17a529858
Rachala Ovin <resistlyfe@gmail.com>
2021-12-13 11:05:23 +0530

    Expermental Permission types

--- src/utils/auth/types.ts
+++ src/utils/auth/types.ts
@@ -8,11 +8,36 @@ export interface LoginResponse extends Response {
   errors?: Error[];
 }
 
+type PermType =
+  | "manga"
+  | "chapter"
+  | "author"
+  | "scanlation_group"
+  | "cover"
+  | "user"
+  | "manga_relation";
+type PermSelf = "view" | "list";
+
+/**
+ * **Experimental**
+ *
+ * This type doesn't have all the types that the server could
+ * respond with.
+ */
+type Permission = `${PermType}.${PermSelf}`;
+/**
+ * **Experimental**
+ *
+ * This type doesn't have all the types that the server could
+ * respond with.
+ */
+type Role = "ROLE_GUEST" | "IS_ANONYMOUS" | "IS_AUTHENTICATED_ANONYMOUSLY";
+
 export interface CheckResponse extends Response {
   result: "ok";
   isAuthenticated: boolean;
-  roles: string[];
-  permissions: string[];
+  roles: Role[];
+  permissions: Permission[];
 }
 
 export interface RefreshResponse extends LoginResponse {
